db6655379ebfb6472f83ff8a0032466b399d047b
avisas <alejandra.visa91@gmail.com>
2019-06-27 13:15:46 -0500

    coloco display-flex al css

--- src/main.js
+++ src/main.js
@@ -1,1 +1,12 @@
 /* Manejo del DOM */
+
+const btnSubmit = document.getElementById('inputSubmit');       
+btnSubmit.addEventListener("click", () => {
+    const enteredUsername = document.getElementById('inputUname').value;
+    const enteredPassword = document.getElementById('inputPsw').value;
+    if (enteredUsername == "laboratoria" && enteredPassword == "LABORATORIA") {
+        alert('login correcto');
+    } else {
+        alert('Usuario y/o contraseña incorrectos');
+        }
+    });
