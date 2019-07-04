6f23a19bf16c9d8b3e8c1d18d3d2287dade56818
avisas <alejandra.visa91@gmail.com>
2019-07-04 12:21:42 -0500

    actualizado main

--- src/main.js
+++ src/main.js
@@ -1,45 +1,23 @@
 /* Manejo del DOM */
 const contLogin = document.getElementById('cont-login');    
 const btnSubmit = document.getElementById('inputSubmit');    
-const pokedex = document.getElementById('pokedex');
-const datosPokemones = POKEMON.pokemon;
-let iconsTipo='';
-let esNull='';
+const enteredUsername = document.getElementById('inputUname').value;
+const enteredPassword = document.getElementById('inputPsw').value;
+
+const dataPokemon = POKEMON.pokemon;
 
  // Login  
 btnSubmit.addEventListener("click", () => {
-    const enteredUsername = document.getElementById('inputUname').value;
-    const enteredPassword = document.getElementById('inputPsw').value;
-    if (enteredUsername == "laboratoria" && enteredPassword == "LABORATORIA") {
-        pokedex.classList.add("flex");
-        contLogin.classList.add("none");
-    } else {
-        }
-    });
-
-for (i = 0; i < datosPokemones.length; i++) { 
-	let pokemon = datosPokemones[i];
-	let cantMultipliers=0;
-	let tiposPokemon = pokemon.type;
-
-	if (pokemon.multipliers===null) {
-		esNull='por-atrapar';
-	}else{
-		cantMultipliers = pokemon["multipliers"].length;
+	if (enteredUsername === "" && enteredPassword === "") {
+		pokedex.classList.add("flex");
+		contLogin.classList.add("none");
 	}
-	for (n = 0; n < tiposPokemon.length; n++) {
-     iconsTipo +=`<img src="img/icon-${tiposPokemon[n]}.png" alt="${tiposPokemon[n]}">`;
-   }
-	
-	pokedex.innerHTML +=`<div class="content-pokemones ${esNull}">
-          <span class="cant-multipliers">x${cantMultipliers}</span>
-          <img class="img-pokemon" src="${pokemon.img}">
-          <div class="contenido-poke">
-            <h2 class="nombre-pokemon">${pokemon.name}</h2>
-            <p class="num-pokemon">#${pokemon.num}</p>
-            <div class="cont-tipo">${iconsTipo}</div>
-          </div>
-        </div>`;
-  iconsTipo='';
-  esNull='';
-}
+});
+
+// console.log(example(dataPokemon)); //array
+
+// const arrayUno = example(dataPokemon);
+
+// for(let i = 0; i < arrayUno.length; i++){
+// 	l<
+// }
